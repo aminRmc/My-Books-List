513b4e7bdf74bd5c060f6ac9a67c5609e0dfdf21
aminRmc <ramcilovicamin@gmail.com>
2024-04-05 12:13:27 +0200

    add v55

--- src/app/Profile/page.js
+++ src/app/Profile/page.js
@@ -7,50 +7,44 @@ import { removefav } from '@/store/slice/connexion';
 
 function Page() {
   const user = useSelector((state) => state.connexion.user);
-  const dispatch = useDispatch(); 
-
-
+  const dispatch = useDispatch();
 
   return (
-    <section className='w-full flex flex-col  items-center'>
-    <Navbar/>                                           
-    <div className='w-full flex gap-10 justify-center items-center pt-20'>
-        <div className='h-[150px] w-[150px] rounded-full bg-red-700'    >
+    <section className='w-full flex flex-col items-center'>
+      <Navbar />
+      <div className='w-full flex gap-10 justify-center items-center pt-20'>
+        <div className='h-[150px] w-[150px] rounded-full bg-red-700'>
         </div>
         <div className='flex flex-col gap-2'>
-        <button className="btn rounded-3xl" onClick={()=>document.getElementById('my_modal_2').showModal()}>change avatar</button>
-        <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-       
+          <button className="btn rounded-3xl" onClick={() => document.getElementById('my_modal_2').showModal()}>change avatar</button>
+          <dialog id="my_modal_2" className="modal">
+            <div className="modal-box">
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+          <p> User : {user?.[0]?.user || 'N/A'}</p>
+          <p> Mail : {user?.[0]?.mail || 'N/A'}</p>
         </div>
-        <form method="dialog" className="modal-backdrop">
-        <button>close</button>
-        </form>
-        </dialog>
-        <p> User : {user[0].user}</p>
-        <p> Mail : {user[0].mail}</p>
-        </div>
-    </div>
+      </div>
       <div className='w-1/2 flex flex-col pt-20'>
         <h1>My Fav</h1>
         <hr className='border-secondary border-solid border-1 w-full' />
         <div className='grid gap-2 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-7  p-4'>
-          {user[0].fav.map((value, index) => {
-            return (
-              <div key={value.id} className="relative">
-                <Link href={`/Books/${value.id}`}>
-                  <img className='h-[150px] w-[110px]' src={value.image_url} alt="" />
-                </Link>
-                {/* Ajouter une croix pour supprimer l'élément */}
-                <button 
-                  className="absolute top-0 right-0 bg-red-500 text-white rounded-full p-1"
-                  onClick={() => {dispatch(removefav(index))}}
-                >
-                  X
-                </button>
-              </div>
-            )
-          })}
+          {user?.[0]?.fav?.map((value, index) => (
+            <div key={value.id} className="relative">
+              <Link href={`/Books/${value.id}`}>
+                <img className='h-[150px] w-[110px]' src={value.image_url} alt="" />
+              </Link>
+              <button
+                className="absolute top-0 right-0 bg-red-500 text-white rounded-full p-1"
+                onClick={() => { dispatch(removefav(index)) }}
+              >
+                X
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </section>
