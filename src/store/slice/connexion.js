3cc00572482ea33d6c24abd6193ae1c6115bbb8d
aminRmc <ramcilovicamin@gmail.com>
2024-04-05 12:08:10 +0200

    add v54

--- src/store/slice/connexion.js
+++ src/store/slice/connexion.js
@@ -1,32 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 // Function to load state from localStorage
-const loadFromLocalStorage = () => {
-  if (typeof window !== 'undefined') {
-    const serializedState = localStorage.getItem('formState');
-    // If there is no saved state, return a default state object
-    if (serializedState === null) return { value: 1, tab: [], con: 0, user: [] };
-    // Parse the saved state and return it
-    return JSON.parse(serializedState);
-  } else {
-    // Return the default initial state if executed on the server side
-    return { value: 1, tab: [], con: 0, user: [] };
-  }
-};
+// const loadFromLocalStorage = () => {
+//   if (typeof window !== 'undefined') {
+//     const serializedState = localStorage.getItem('formState');
+//     // If there is no saved state, return a default state object
+//     if (serializedState === null) return { value: 1, tab: [], con: 0, user: [] };
+//     // Parse the saved state and return it
+//     return JSON.parse(serializedState);
+//   } else {
+//     // Return the default initial state if executed on the server side
+//     return { value: 1, tab: [], con: 0, user: [] };
+//   }
+// };
 
-// Function to save state to localStorage
-const saveToLocalStorage = (state) => {
-  if (typeof window !== 'undefined') {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('formState', serializedState);
-  } else {
-    console.log("localStorage is not available in this environment.");
-    // Consider alternative persistent state management here
-  }
-};
+// // Function to save state to localStorage
+// const saveToLocalStorage = (state) => {
+//   if (typeof window !== 'undefined') {
+//     const serializedState = JSON.stringify(state);
+//     localStorage.setItem('formState', serializedState);
+//   } else {
+//     console.log("localStorage is not available in this environment.");
+//     // Consider alternative persistent state management here
+//   }
+// };
 
 // Load the initial state from localStorage
-const initialState = loadFromLocalStorage();
+const initialState ={value: 1, tab: [], con: 0, user: []};
+//  loadFromLocalStorage();
+
 
 // Create a slice with reducers to handle actions
 export const connexion = createSlice({
@@ -36,36 +38,36 @@ export const connexion = createSlice({
     // Actions...
     register: (state, action) => {
       state.tab.push(action.payload);
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
     login: (state) => {
       state.value = 0;
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
     reg: (state) => {
       state.value = 1;
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
     connect: (state) => {
       state.con = 1;
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
     disconnect: (state) => {
       state.con = 0;
       state.user = [];
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
     info: (state, action) => {
       state.user.push(action.payload);
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
     addfav: (state, action) => {
       state.user[0].fav.push(action.payload);
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
     removefav: (state, action) => {
       state.user[0].fav.splice(action.payload, 1);
-      saveToLocalStorage(state);
+      // saveToLocalStorage(state);
     },
   },
 });
